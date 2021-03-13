eaca7a24524941e32f5ffe7bc01a0e72ac66b283
Tim Keresey <tim.keresey@gmail.com>
2021-03-12 22:30:51 -0700

    dispatch fetch articles action

--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 
 import Card from '../components/Card';
+import * newsAction from '../reduc/actions/newsAction';
 
 
 const NewsListScreen = (props) => {
 
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(newsAction.fetchArticles())
+    }, [dispatch]);
+
+    const articles = useSelector(state => state.news.articles);
+
     return (
         <Card navigation={props.navigation} />
-    );
-        
+    );     
 }
 
 const styles = StyleSheet.create({
