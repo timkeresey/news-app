5f40790a4e744360aa6f5da934fad8344cbbfa8c
Tim Keresey <tim.keresey@gmail.com>
2021-03-12 18:01:00 -0700

    add bottom tab navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import NewsListScreen from '../screens/NewsListScreen';
 import NewsDetailsScreen from '../screens/NewsListScreen';
+import FavoritesScreen from '../screens/FavoriteScreen';
 
 const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+function HomeNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+                name='NewsList' 
+                component={NewsListScreen} 
+                options={{title: 'All News'}}
+            />
+            <Stack.Screen 
+                name='NewsDetails' 
+                component={NewsDetailsScreen} 
+                options={{title: 'News Details'}}
+            />
+        </Stack.Navigator>
+    );
+}
 
 function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen 
-                    name='NewsList' 
-                    component={NewsListScreen} 
-                    options={{title: 'All News'}}
-                />
-                <Stack.Screen 
-                    name='NewsDetails' 
-                    component={NewsDetailsScreen} 
-                    options={{title: 'News Details'}}
-                />
-            </Stack.Navigator>
+            <Tabs.Navigator>
+                <Tabs.Screen name='Home' component={HomeNavigator} />
+                <Tabs.Screen name='Favorites' component={FavoritesScreen} />
+            </Tabs.Navigator>
         </NavigationContainer>
     );
 }
