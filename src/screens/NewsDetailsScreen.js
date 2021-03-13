5005d880f0f05eae1dde6f9107b89ba4e1cf7fcc
Tim Keresey <tim.keresey@gmail.com>
2021-03-13 16:50:24 -0700

    add fav icon to details screen

--- src/screens/NewsDetailsScreen.js
+++ src/screens/NewsDetailsScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {StyleSheet, View, Text, ImageBackground} from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+import * as newsAction from '../redux/actions/newsAction';
 
 const NewsDetailsScreen = (props) => {
 
+    const dispatch = useDispatch();
     const {articleUrl} = props.route.params;
-     const article = useSelector(state => state.news.articles.articles.find(article => article.url === articleUrl));
+    const article = useSelector(state => state.news.articles.articles.find(article => article.url === articleUrl));
+    const isFav = useSelector(state => state.news.favorites.some(article => article.url === article.url));
+
 
     return (
         <View style={styles.container}>
@@ -21,7 +26,14 @@ const NewsDetailsScreen = (props) => {
                         <Text style={styles.author}>
                             {article.author}
                         </Text>
-                        <MaterialIcons />
+                        <MaterialIcons 
+                            name={isFav ? 'favorite' : 'favorite-border'} 
+                            color='#72bcd4' 
+                            size={24} 
+                            onPress={() => {
+                                dispatch(newsAction.toggleFavorites(article.url))
+                            }}
+                    />
                     </View>
                 </ImageBackground>
             </View>
